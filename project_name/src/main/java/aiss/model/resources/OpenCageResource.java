6fb330a210237f721278781c0dc8a642c0e8dda4
TGB07 <63409689+TGB07@users.noreply.github.com>
2020-04-27 17:51:09 +0200

    Insercion null en OpenCageResource

--- project_name/src/main/java/aiss/model/resources/OpenCageResource.java
+++ project_name/src/main/java/aiss/model/resources/OpenCageResource.java
@@ -6,6 +6,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.restlet.resource.ClientResource;
+import org.restlet.resource.ResourceException;
 
 import aiss.model.opencage.LLNameSearch;
 
@@ -13,16 +14,24 @@ public class OpenCageResource {
 
 	private static final String OpenCage_API_KEY = "07cecfd3cd97455b87fae72871cefe05";
 	private static final Logger log = Logger.getLogger(OpenCageResource.class.getName());
-	
+
 	public LLNameSearch getLatitudLongitud(String query) throws UnsupportedEncodingException {
 
+		ClientResource cr = null;
+		LLNameSearch llnameSearch=null;
+		
 		String queryFormatted = URLEncoder.encode(query, "UTF-8");
-		String uri = "http://api.opencagedata.com/geocode/v1/json?q=" + queryFormatted +"&key=" + OpenCage_API_KEY + "&language=es&pretty=1&no_annotations=1&limit=1&min_confidence=3";
+		String uri = "https://api.opencagedata.com/geocode/v1/json?q=" + queryFormatted +"&key=" + OpenCage_API_KEY + "&language=es&pretty=1&no_annotations=1&limit=1&min_confidence=3";
 		
 		log.log(Level.FINE, "OpenCage URI: " + uri);
 		
-		ClientResource cr = new ClientResource(uri);
-		LLNameSearch llnameSearch = cr.get(LLNameSearch.class);
+		try {
+			cr = new ClientResource(uri);
+			llnameSearch = cr.get(LLNameSearch.class);
+		}
+		catch (ResourceException re){
+			System.err.println("Error when retrieving the latitude/longitude" + cr.getResponse().getStatus());
+		}
 		
 		return llnameSearch;
 	}
