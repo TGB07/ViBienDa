b12af4e0fe805345a98f47e500e742ae5bfe4798
TGB07 <63409689+TGB07@users.noreply.github.com>
2020-05-20 20:54:16 +0200

    #Controlador ListasDelUsuario

--- project_name/src/main/java/aiss/controller/GetAllUserListsController.java
+++ project_name/src/main/java/aiss/controller/GetAllUserListsController.java
@@ -1,6 +1,10 @@
 package aiss.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -10,6 +14,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import aiss.model.foursquare.FoursquareToken;
+import aiss.model.foursquare.listD.Response;
+import aiss.model.foursquare.list.FoursquareList;
+import aiss.model.foursquare.list.Item_;
+import aiss.model.foursquare.listD.FoursquareListDetails;
 import aiss.model.resources.FoursquareResource;
 
 public class GetAllUserListsController extends HttpServlet {
@@ -31,17 +39,46 @@ public class GetAllUserListsController extends HttpServlet {
 		
 		String code = request.getParameter("code");
 		
-		FoursquareResource fsResource = new FoursquareResource();
-		FoursquareToken fsAcessToken= fsResource.getFoursquareAccessToken(code);
+		request.setAttribute("code", code);
 		
-		String accessToken = fsAcessToken.getAccessToken();
+		if(code!=null) {
+			
+			FoursquareResource fsResource = new FoursquareResource();
+			FoursquareToken fsAcessToken= fsResource.getFoursquareAccessToken(code);
+			
+			String accessToken = fsAcessToken.getAccessToken();
+			request.setAttribute("accessToken", accessToken);
+						
+			log.log(Level.FINE, "AccessToken retrieved " + accessToken);
+					
+			FoursquareList fl = fsResource.getUserLists(accessToken);
+			
+			List<Response> detallesListaUsuario = new ArrayList<Response>();
+			Map<String, String> infoListasDelUsuario = new HashMap<String, String>();//clave: id lista; valor: nombre lista 
+			List<Item_> listaDelUsuario = fl.getResponse().getLists().getItems();
+			
+			for (int i = 0; i < listaDelUsuario.size(); i++) {
+				String id= listaDelUsuario.get(i).getId();
+				String nLista= listaDelUsuario.get(i).getName();
+				infoListasDelUsuario.put(id, nLista);
+				detallesListaUsuario.add(fsResource.getVenuesList(accessToken, id).getResponse());
+			}
+			
+			request.setAttribute("infoListasDelUsuario", infoListasDelUsuario);
+			request.setAttribute("listaDelUsuario", listaDelUsuario);
+			request.setAttribute("detallesListaUsuario", detallesListaUsuario);
+			
+			
+			// Forward view
+			request.getRequestDispatcher("/userVenuesView.jsp").forward(request, response);;
+		}
 		
-		request.setAttribute("accessToken", accessToken);
+		else {
+			log.log(Level.FINE, "Accediendo a usuario sin token, se devuelve a la vista de datos de nuevo");
+			request.getRequestDispatcher("/generalStatsView.jsp").forward(request, response);
+		}
 		
-		log.log(Level.FINE, "AccessToken retrie ved " + accessToken);
-		
-		// Forward view
-		request.getRequestDispatcher("/userVenuesView.jsp").forward(request, response);;
+
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
