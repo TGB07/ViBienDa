871256ee76fe99ed0e8c82345fe5ad234b9538dd
acampillos <49621990+acampillos@users.noreply.github.com>
2020-05-20 23:09:27 +0200

    Update minimapa.

--- project_name/src/main/webapp/js/venuesMiniMap.js
+++ project_name/src/main/webapp/js/venuesMiniMap.js
@@ -1,5 +1,5 @@
 /*MINIMAPA DE GENERAL STATS VIEW*/
-var map = L.map('map').setView([lat, lon], 8);
+var map = L.map('map').setView([lat, lon], 10);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -8,31 +8,13 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 L.control.scale().addTo(map);
 
-alert(venues[0].name);
-
-//var searchControl = new L.esri.Controls.Geosearch().addTo(map);
-//
-//var results = new L.LayerGroup().addTo(map);
-//
-//searchControl.on('results', function(data){
-//	results.clearLayers();
-//});
-//
-//var popup = L.popup();
-//
-//function onMapClick(e){
-//	// + "(" + e.latlng.lat.toString().substring(0, 5) + "," + e.latlng.lng.toString().substring(0, 5) + ")"
-//	popup
-//		.setLatLng(e.latlng)
-//		.setContent('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">'+
-//				"Seleccione el radio en kilómetros:" +
-//				'<form class="search" action="SearchController" method="post">' +
-//				'<input id="radio" name="radio" type="number" min="0" max="100" step="1" value="10">' +
-//				'<input name="lat" type="hidden" value="' + e.latlng.lat + '">' +
-//				'<input name="lon" type="hidden" value="' + e.latlng.lng + '">' +
-//				'<button type="submit"><i class="fa fa-arrow-right"></i></button>' +
-//				'</form>')
-//		.openOn(map);
-//}
-//
-//map.on('click', onMapClick);
+var marker;
+var venue;
+var popup;
+for(var i = 0; i<venues.length; i++){
+	venue = venues[i];
+	marker = L.marker([venue.lat, venue.lon])
+		.addTo(map);
+	marker.bindPopup(venue.name)
+		.openPopup();
+}
--- project_name/src/main/webapp/userVenuesView.jsp
+++ project_name/src/main/webapp/userVenuesView.jsp
@@ -6,11 +6,49 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+
 <title>UserVenuesStatsView</title>
 </head>
+
 <body>
 
+	<div class="wrapper">
+		<!-- NAV-BAR -->
+		<jsp:include page="navbar.html" />
 
+		<!-- Listas del usuario -->
+		<div class="container">
+			<h2>Mis listas de lugares favoritos:</h2>
+			<h2>NOMBRE DEL USUARIO</h2>
+			<p>Seleccione la lista de la que desea obtener sus lugares guardados.</p>
+			
+			<div class="panel-group" id="accordion">
+			
+				<!--  -->
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h4 class="panel-title">
+							<a data-toggle="collapse" data-parent="#accordion"
+								href="#collapse1">AQUI ES UN c:out</a>
+						</h4>
+					</div>
+					<div id="collapse1" class="panel-collapse collapse in">
+						<div class="panel-body">
+							<div class="panel-info">
+								c:out DE LA DESCRIPCION, SEGUIDORES, ETC
+							</div>
+							
+							
+							
+						</div>
+					</div>
+				</div>
+				<!--  -->
+			</div>
+		</div>
+
+	</div>
 
 
 
